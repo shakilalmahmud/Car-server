95604b68b36e76f871d932d634ac67cfe51576d8
mdshakilalmahmud <shakil.am1092@gmail.com>
2023-08-15 23:59:42 +0600

    sadsad

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
-console.log(process.env)
+//console.log(process.env)
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ker7tlm.mongodb.net/?retryWrites=true&w=majority`;
 
